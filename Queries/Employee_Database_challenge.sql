9da7924f9ae666188c35043a056b538c3fd4dd86
James Rauschnot <jamesrauschnot@Jamess-MacBook-Pro-2.local>
2022-02-10 18:00:09 -0800

    Challenge upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,23 +1,49 @@
--- Start of Challenge Material 
+--Deliverable 1 - The Number of Retiring Employees by Title
+SELECT e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t ON e.emp_no = t.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
 
-SELECT * FROM employees
-SELECT * FROM deliverable1_table1
-SELECT * FROM deliverable1_table2
+SELECT * FROM retirement_titles
 
---Deliverable 1 table 1 
-SELECT emp_no, first_name, last_name
-INTO deliverable1_table1
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31');
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (r.emp_no) 
+r.emp_no, r.first_name, r.last_name, r.title
+INTO unique_titles
+FROM retirement_titles as r
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no ASC, to_date DESC;
 
---Deliverable 1 table 2
-SELECT title, from_date, to_date
-INTO deliverable1_table2
-FROM titles;
+SELECT * FROM unique_titles
 
---Join table 1 with table 2 on PK
-SELECT t1.emp_no, t1.first_name, t1.last_name,
-titles.emp_no, titles.title, titles.from_date, titles.to_date
-FROM deliverable1_table1 as t1
-INNER JOIN titles
-ON t1.emp_no = titles.emp_no;
+--Determine the number of employees who are about to retire by job title
+SELECT COUNT(title), title
+INTO no_retiring_titles
+FROM unique_titles 
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT * FROM no_retiring_titles
+
+--Determine number of employees to retire by job title
+SELECT COUNT(title), title
+INTO no_retiring_titles
+FROM unique_titles 
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT * FROM no_retiring_titles
+
+--Deliverable 2: Employees eligible for Mentorship Program
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, 
+de.from_date, de.to_date, t.title
+INTO ment_elig
+FROM employees as e
+JOIN titles as t ON (e.emp_no = t.emp_no)
+JOIN dept_emp as de ON (e.emp_no = de.emp_no)
+WHERE (de.to_date = '9999-01-01') 
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+
+SELECT * FROM ment_elig
--- Queries/schema.sql
+++ Queries/schema.sql
@@ -1,30 +1,36 @@
 -- Creating tables for PH-EmployeeDB
-CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
-     dept_name VARCHAR(40) NOT NULL,
-     PRIMARY KEY (dept_no),
-     UNIQUE (dept_name)
+CREATE TABLE departments(
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
 );
 
-CREATE TABLE employees (
+SELECT * FROM departments
+
+CREATE TABLE employees(
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	gender VARCHAR NOT NULL,
-	hire_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    gender VARCHAR NOT NULL,
+    hire_date DATE NOT NULL,
+    PRIMARY KEY (emp_no)
 );
+
+SELECT * FROM employees
 
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
+
+SELECT * FROM dept_manager
 
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
@@ -35,23 +41,27 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_emp(
-emp_no INT NOT NULL,
-dept_no VARCHAR(4) NOT NULL,
-from_date DATE NOT NULL,
-to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-PRIMARY KEY (emp_no, dept_no, from_date, to_date)
+SELECT * FROM salaries
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
+
+SELECT * FROM dept_emp
 
 CREATE TABLE titles (
-emp_no INT NOT NULL,
-title VARCHAR(40) NOT NULL,
-from_date DATE NOT NULL,
-to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (emp_no, title)
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
-SELECT * FROM departments;
+SELECT * FROM titles
+
